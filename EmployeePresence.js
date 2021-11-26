59337ac88d537cc1255f660e0aa155255249589e
9908229348 <kalavaisravanthi98@gmail.com>
2021-11-26 17:13:46 +0530

    Refactored: saving daily wage in  an array

--- EmployeePresence.js
+++ EmployeePresence.js
@@ -74,3 +74,24 @@ while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
 empWage = totalEmpHrs * WAGE_PER_HOUR;
 console.log("Total Days :" + totalWorkingDays + " Total Hrs : " + totalEmpHrs + "Emp Wage : " + empWage);
 
+// Uc6
+console.log("Uc6");
+function calcDailyWage(empHrs){
+    return empHrs * WAGE_PER_HOUR;
+}
+
+totalEmpHrs = 0;
+totalWorkingDays = 0;
+let empDailyWageArr = new Array();
+
+while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
+    totalWorkingDays++;
+    empCheck = Math.floor(Math.random() * 10) % 3;
+    empHrs = getWorkingHours(empCheck);
+    totalEmpHrs += empHrs;
+    empDailyWageArr.push(calcDailyWage(empHrs));
+}
+
+empWage = calcDailyWage(totalEmpHrs);
+console.log(empDailyWageArr);
+console.log("Total Days : " + totalWorkingDays + " Total Hrs : " + totalEmpHrs + " Emp Wage : " + empWage);
