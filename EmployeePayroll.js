8c287164e946fe5424b6f77475cee2b56e666204
9908229348 <kalavaisravanthi98@gmail.com>
2021-11-28 10:32:03 +0530

    Refactored: Extend EmployeePayroll with gender,startDate

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -1,18 +1,24 @@
 class EmployeePayrollData {
     id;
     salary;
+    gender;
+    startDate;
 
-    constructor(id, name, salary){
-        this.id = id;
-        this.name = name;
-        this.salary = salary;
+    constructor(...parameters){
+        this.id = parameters[0];
+        this.name = parameters[1];
+        this.salary = parameters[2];
+        this.gender = parameters[3];
+        this.startDate = parameters[4];
     }
 
     get name(){ return this._name; }
     set name(name){ this._name = name; }
 
     toString(){
-        return "id = " + this.id + ", name = " + this.name + ", salary = " + this.salary;
+        const options = { year: 'numeric', month: 'long', day: 'numeric'};
+        const empDate = this.startDate === undefined ? "undefined": this.startDate.toLocaleDateString("en-US", options);
+        return "id = " + this.id + ", name = " + this.name + ", salary = " + this.salary + ", gender = " + this.gender + ", startDate = " + empDate;
     }
 }
 
@@ -20,3 +26,5 @@ let employeePayrollData = new EmployeePayrollData(1, "Mark", 30000);
 console.log(employeePayrollData.toString());
 employeePayrollData.name = "john";
 console.log(employeePayrollData.toString());
+let newEmployeePayrollData = new EmployeePayrollData(1, "Terrisa", 30000, "F", new Date());
+console.log(newEmployeePayrollData.toString());
